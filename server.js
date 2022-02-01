67c0938f8d1fb0559e788eaa7d992a97ec11f511
Hector Nava <hector.r.nava44@gmail.com>
2022-02-01 15:31:35 -0700

    Added index.js files for APIs and comments

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+/*
+We are going to build our folders and API's in such a way
+where our folder structure will match our endpoints.
+This is accomplished by using "routing middleware"
+
+/api/books
+--
+routes
+    api
+        bookRoutes
+*/
+
+
+
+
 // connect to the database prior to starting our server
 // {force: true} > Force the database to drop/recreate the table
 // whenever we start/restart our server (NEVER DO IN PROD)
