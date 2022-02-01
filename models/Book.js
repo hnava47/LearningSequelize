7b4cc8bf21b8cc18c196661ca4df66be0e4e7192
Hector Nava <hector.r.nava44@gmail.com>
2022-02-01 11:44:13 -0700

    Created Book model

---
+++ models/Book.js
@@ -0,0 +1,41 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config');
+
+// Declares a class called book and inherits
+// all the prototypes methods that are in the model class
+// from sequelize
+class Book extends Model {}
+
+// Creates table using JavaScript
+Book.init(
+    // Declares columns for this table
+    // By default, sequelize creates the id column for us
+    {
+        title: {
+            type: DataTypes.STRING
+        },
+        author: {
+            type: DataTypes.STRING
+        },
+        isbn: {
+            type: DataTypes.STRING
+        },
+        pages: {
+            type: DataTypes.INTEGER
+        },
+        edition: {
+            type: DataTypes.INTEGER
+        },
+        isPaperBack: {
+            type: DataTypes.BOOLEAN
+        }
+    },
+    {
+        // Which database connection should this table be created in
+        sequelize,
+        // By default sequelize will create 2 columns for our table
+        // created_at and updated_at (not being added when we pass false)
+        timestamps: false,
+        modelName: 'book'
+    }
+);
