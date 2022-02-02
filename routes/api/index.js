8bde9080e4de62d1130e473f5e5a1e3c20d8d9c6
Hector Nava <hector.r.nava44@gmail.com>
2022-02-02 10:37:56 -0700

    Added /user route to api index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const bookRoutes = require('./bookRoutes');
+const userRoutes = require('./userRoutes');
 
 // Every route inside of this index.js
 // already has /api prepended to every route we declare
 
 // This will prepend /api/books to every route declared below this comment
 router.use('/books', bookRoutes);
+
+// This will prepend /api/users to every route declared below this comment
+router.use('/users', userRoutes);
 
 module.exports = router;
